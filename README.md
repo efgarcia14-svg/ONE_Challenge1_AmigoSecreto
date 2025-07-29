cc97bddeec548b12fefbfe5022cccceaea7fcd11
Eva_ONE9G <efgarcia14@gmail.com>
2025-07-29 00:38:27 -0600

    Se reorganizaron los archivos y se realizaron algunas correcciones en el código JavaScript del archivo `app.js` para mejorar la funcionalidad del sorteo de amigos secretos. Además, se limpiaron las listas de amigos después de cada sorteo.

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# ONE-firstSteps
-Primera etapa del programa Oracle Next Education
+# ONE-Amigo Secreto Challenge
+Este desafío está diseñado para desarrollar la funcionalidad de una aplicación que permita a los usuarios ingresar nombres de sus amigos en una lista y que luego a través de un sorteo aleatorio determinar quién es el AMIGO SECRETO.
+
+La interfaz incluye:
+1. Una caja de texto que permite capturar y poblar un listado de nombres a través de un botón.
+2. Una lista que permite vvisualizar los nombres con los cuales se realizará el sorteo.
+3. Un botón para ejecutar el sorteo aleatorio.
+
+<video controls src="20250729-0438-27.4107356.mp4" title="InterfazDeUso"></video>
+
+Validaciones de Entrada de Datos:
+Adicionalmente la aplicación valida si la caja de texto tiene un valor válido (sólo letras), y también si el listado tiene al menos dos nombres agregados para realizar el sorteo.
+
+Funcionalidades:
+Requeridad:
+1. sortearAmigo()
+Función que realiza la selección aleatoria entre el listado de nombres disponibles para el sorteo. Antes de realizar la ejecución valida que el listado se encuentre con al menos dos nombres.
+
+2. validaListado()
+Valida la cantidad de elementos agregados a la lista sea al menos dos para poder realizar el sorteo
+
+3. 
+
+
+agregarAmigo()
+Función para capturar y adicionar los nombres a la lista cuya ejecución se dispara al momento de presionar el botón "Añadir".
+
+
+
+
--- app.js
+++ app.js
@@ -1,61 +1,75 @@
 //Inicialización de variables globales
-const resultados = document.getElementById('resultado');
-const titular = document.getElementById('titular');
 const lista = document.getElementById('listaAmigos');
+const resultados = document.getElementById('resultado');
+ const amigosListados = [];
+const titular = document.getElementById('titular');
 const divProgreso = document.getElementById("barra");
+
+// Función para sortear un amigo secreto
+// Esta función verifica si hay al menos dos amigos en la lista antes de realizar el sorteo
+function sortearAmigo() {    
+    if (amigosListados.length < 2) {
+        alert('Necesitas al menos dos amigos para realizar el sorteo.');
+        resetearJuego(); // Limpiar resultados previos y reiniciar el juego
+        return;
+    }
+    generarResultado(); // Mostrar el resultado del sorteo   
+}
+
+// Función para obtener la lista de amigos
+// Esta función recorre los elementos de la lista y los almacena en un array
+function obtenerListadoAmigos() {
+    lista.textContent = ''; // Limpiar la lista antes de poblarla
+        for (let i = 0; i < amigosListados.length; i++) {
+        const itemLista = document.createElement('li');
+        itemLista.textContent = amigosListados[i];
+           lista.appendChild(itemLista); 
+    }             
+}
+
+function generarResultado(listadoAmigos) {
+    animateProgressBar(document.getElementById("progresoBarra").clientWidth);// Animar la barra de progreso
+    sorteo = Math.floor(Math.random() * amigosListados.length);// Seleccionar un amigo al azar
+    setTimeout(() => {// Esperar 2 segundos antes de mostrar el resultado
+        divProgreso.style.visibility = "hidden"; // Ocultar la barra de progreso
+        resultados.textContent = `Ha resultado sorteado: ${amigosListados[sorteo]}`;
+        titular.textContent = 'Puedes volver agregando una nueva lista de amigos!';       
+        lista.textContent = ''; // Limpiar la lista de amigos
+    }, 2000);
+
+}
 
 // función para agregar un amigo a la listaz
 // Esta función toma el valor del input, lo convierte a mayúsculas y lo agrega a la lista
 function agregarAmigo() {
     const input = document.getElementById('amigo');
     const amigo = input.value.trim().toUpperCase();
-    if (amigo) {
-        const amigosList = document.createElement('li');
-        amigosList.textContent = amigo;
-        lista.appendChild(amigosList);
-        input.value = '';
+    if (validarNombre(amigo)) {
+        poblarListadoAmigos(amigo); // Llamar a la función para poblar la lista de amigos|          
     } else {
         alert('Por favor, ingresa un nombre válido.');
-    }
-    resetearJuego(); // Limpiar resultados previos y reiniciar el juego
+    }   
+    input.value = ''; 
+    obtenerListadoAmigos(); // Actualizar la lista de amigos
 };
 
-// Función para obtener la lista de amigos
-// Esta función recorre los elementos de la lista y los almacena en un array
-function obtenerListadoAmigos() {
-    const listaAmigos = [];
-    for (let i = 0; i < lista.children.length; i++) {
-        listaAmigos[i] = lista.children[i].textContent.trim().toUpperCase();
-    }
-    return listaAmigos;
+function validarNombre(texto) {
+  const regex = /^[a-zA-Z]+$/;
+  return regex.test(texto);
 }
 
-// Función para sortear un amigo secreto
-// Esta función verifica si hay al menos dos amigos en la lista antes de realizar el sorteo
-function sortearAmigo() {
-    const listadoAmigos = obtenerListadoAmigos(); // Obtener la lista de amigos
-    if (listadoAmigos.length < 2) {
-        alert('Necesitas al menos dos amigos para realizar el sorteo.');
-        resetearJuego(); // Limpiar resultados previos y reiniciar el juego
-        return;
-    }
-    mostrarResultado(listadoAmigos); // Mostrar el resultado del sorteo   
-
+function poblarListadoAmigos(amigo) {   
+   
+        if (amigosListados.includes(amigo)) {
+            alert('Este amigo ya está en la lista.');
+            return;
+        }     
+        amigosListados.push(amigo); // Agregar el amigo a la lista
 }
 
 // Función para mostrar el resultado del sorteo
 // Esta función selecciona un amigo al azar de la lista y muestra el resultado en la interfaz
-function mostrarResultado(listadoAmigos) {
-    animateProgressBar(document.getElementById("progresoBarra").clientWidth);// Animar la barra de progreso
-    sorteo = Math.floor(Math.random() * listadoAmigos.length);// Seleccionar un amigo al azar
-    setTimeout(() => {// Esperar 2 segundos antes de mostrar el resultado
-        resultados.textContent = `Ha resultado sorteado: ${listadoAmigos[sorteo]}`;
-        lista.textContent = '';
-        titular.textContent = 'Puedes volver agregando una nueva lista de amigos!';
-        divProgreso.style.visibility = "hidden"; // Ocultar la barra de progreso
-    }, 2000);
 
-}
 
 // Función para animar la barra de progreso
 // Esta función utiliza setInterval para incrementar el ancho de la barra de progreso hasta un valor final
@@ -87,8 +101,9 @@ function animateProgressBar(width) {
 function resetearJuego() {
     titular.textContent = 'Digite el nombre de sus amigos!';
     resultados.textContent = ''; // Limpiar resultados previos
-    while (lista.length > 0) {// Limpiar la lista de amigos
-        lista.pop();
+    while (amigosListados.length > 0) {// Limpiar la lista de amigos
+        amigosListados.pop();
     }
+    lista.textContent = ''; // Limpiar la lista de amigos
 }
 
