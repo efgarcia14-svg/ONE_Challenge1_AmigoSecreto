f09059d392c8e8f6e451430a26ce76adc298680f
Eva_ONE9G <efgarcia14@gmail.com>
2025-07-29 13:16:45 -0600

    Commit 3: En este se adicionaron cambios para corregir: 1. Se ajustó la función agergarAmigo para que permita verifiar si se realiza un nuevo juego después del sorteo. Se agrego un flag para que permitiera limpiar las variables. 2. Se ajustó las funciones de poblar el array de amigos para que se ajuste al requerimiento. así como generarREsultados para que use ese array. 3. Se modificó la FN validarNombre para que sólo evalue mayúsculas. 4. Se modificó la FN poblarListadoAmigos para que verificará inputs duplicados y no los permita. 5. Se agrego un sort a la función actualizarListadoAmigos.
    Se agregó información al README

--- README.md
+++ README.md
@@ -1,29 +1,44 @@
 # ONE-Amigo Secreto Challenge
 Este desafío está diseñado para desarrollar la funcionalidad de una aplicación que permita a los usuarios ingresar nombres de sus amigos en una lista y que luego a través de un sorteo aleatorio determinar quién es el AMIGO SECRETO.
 
-La interfaz incluye:
-1. Una caja de texto que permite capturar y poblar un listado de nombres a través de un botón.
-2. Una lista que permite vvisualizar los nombres con los cuales se realizará el sorteo.
-3. Un botón para ejecutar el sorteo aleatorio.
+assets\Amigo Secreto Challenge - Eva - ONE9G_v2.mp4
 
-<video controls src="20250729-0438-27.4107356.mp4" title="InterfazDeUso"></video>
+## La interfaz incluye:
+1. Una caja de texto que permite capturar y poblar un listado de nombres a través de un botón Añadir.
+2. Una lista que permite visualizar los nombres con los cuales se realizará el sorteo.
+3. Ejecutar el sorteo aleatorio a través de un botón Sortear Amigo.
+4. Adicionalmente una barra de progreso.
+5. Ayuda o ¿Cómo Funciona?
 
-Validaciones de Entrada de Datos:
-Adicionalmente la aplicación valida si la caja de texto tiene un valor válido (sólo letras), y también si el listado tiene al menos dos nombres agregados para realizar el sorteo.
+## Comportamiento esperado y funcionalidades requeridas
+### 1. Permitir adicionar nombres válidos a una lista que se usará posteriormente para el sorteo.
+```
+#-- agregarAmigo()
+    Gestiona la llamada de las funciones validarNombre, listadoAmigos y actualizarListadoAmigos.
+    Adicionalmente verifica si se ha ejecutado un nuevo juego para limpiar las variables.
+#-- validarNombre(amigo)
+    Valida que el input del nombre no esté vació y que sea solamente letras.
+#-- poblarListadoAmigos()
+    Adiciona amigos al arreglo mientras estos no estén duplicados.
+#-- actualizarListadoAmigos()
+    Actualiza objeto DOM tipo lista con los valores que están en el arreglo odenado alfábeticamente.
+```
 
-Funcionalidades:
-Requeridad:
-1. sortearAmigo()
-Función que realiza la selección aleatoria entre el listado de nombres disponibles para el sorteo. Antes de realizar la ejecución valida que el listado se encuentre con al menos dos nombres.
-
-2. validaListado()
-Valida la cantidad de elementos agregados a la lista sea al menos dos para poder realizar el sorteo
-
-3. 
-
-
-agregarAmigo()
-Función para capturar y adicionar los nombres a la lista cuya ejecución se dispara al momento de presionar el botón "Añadir".
+### 2. Permitir realizar el Sorteo aleatorio para seleccionar dentro de la lista al Amigo Secreto
+```
+#-- sortearAmigo()
+    Verifica si al menos hay dos elementos en la lista de amigos para llamar a la función generarResultado
+    También establece un Flag para saber si hay un nuevo juego.
+#-- generarResultado()
+    Gestiona la llamada a las funciones animatedProgressBar, math.ramdon, math.floor, setTimeOut.
+    Adicionalmente asigna el resultado del sorteo a un objeto DOM.
+#-- animatedProgressBar()
+    Está función dibuja una barra de progreso que se ejecuta en 2 segundos y dibuja el avance a razón de 20px por paso.
+#-- math.ramdon() y math.floor()
+    La primera permitir generar un número aleatorio entre 0 y 1 (excluyente) y la siguiente permite redondear al valor inferior del número generado en la operación.
+#-- setTimeOut()
+    Un temporizador para darle tiempo a la barra de progreso a ejecutarse (2segundos) antes de mostrar el resultado que fue generado.
+```
 
 
 
--- app.js
+++ app.js
@@ -1,65 +1,66 @@
-//Inicialización de variables globales
+// inicialización de variables y elementos del DOM
 const lista = document.getElementById('listaAmigos');
 const resultados = document.getElementById('resultado');
- const amigosListados = [];
 const titular = document.getElementById('titular');
 const divProgreso = document.getElementById("barra");
+const amigosListados = [];
+let flagNuevoSorteo = false; // Bandera para indicar si se ha realizado un nuevo sorteo     
 
-// Función para sortear un amigo secreto
-// Esta función verifica si hay al menos dos amigos en la lista antes de realizar el sorteo
-function sortearAmigo() {    
-    if (amigosListados.length < 2) {
-        alert('Necesitas al menos dos amigos para realizar el sorteo.');
-        resetearJuego(); // Limpiar resultados previos y reiniciar el juego
+
+// función para iniciar el sorteo
+// Esta función verifica si hay al menos dos amigos en la lista antes de realizar el sorteo y generar el resultado al azar
+function sortearAmigo() {          
+     if (amigosListados.length < 2) {
+        alert('Necesitas al menos dos amigos para realizar el sorteo.');               
         return;
     }
     generarResultado(); // Mostrar el resultado del sorteo   
+    flagNuevoSorteo = true; // Cambiar la bandera para indicar que se ha realizado un sorteo
 }
 
-// Función para obtener la lista de amigos
-// Esta función recorre los elementos de la lista y los almacena en un array
-function obtenerListadoAmigos() {
-    lista.textContent = ''; // Limpiar la lista antes de poblarla
-        for (let i = 0; i < amigosListados.length; i++) {
-        const itemLista = document.createElement('li');
-        itemLista.textContent = amigosListados[i];
-           lista.appendChild(itemLista); 
-    }             
-}
-
-function generarResultado(listadoAmigos) {
+// Esta función selecciona un amigo al azar de la lista y muestra el resultado en el DOM y llama a la función para animar la barra de progreso
+// La barra de progreso se muestra durante 2 segundos antes de revelar el resultado del sorteo
+function generarResultado() {
     animateProgressBar(document.getElementById("progresoBarra").clientWidth);// Animar la barra de progreso
     sorteo = Math.floor(Math.random() * amigosListados.length);// Seleccionar un amigo al azar
     setTimeout(() => {// Esperar 2 segundos antes de mostrar el resultado
         divProgreso.style.visibility = "hidden"; // Ocultar la barra de progreso
         resultados.textContent = `Ha resultado sorteado: ${amigosListados[sorteo]}`;
         titular.textContent = 'Puedes volver agregando una nueva lista de amigos!';       
-        lista.textContent = ''; // Limpiar la lista de amigos
-    }, 2000);
-
+        lista.textContent = ''; // Limpiar la lista de amigos        
+    }, 2000);        
 }
 
-// función para agregar un amigo a la listaz
-// Esta función toma el valor del input, lo convierte a mayúsculas y lo agrega a la lista
-function agregarAmigo() {
+// función para agregar un nombre de amigo válido al arreglo de amigos adicionalmente actualiza el DOM de la lista de amigos 
+// y si se ha realizado un nuevo sorteo llama a una función que reinicia las variables del juego
+function agregarAmigo() { 
     const input = document.getElementById('amigo');
-    const amigo = input.value.trim().toUpperCase();
-    if (validarNombre(amigo)) {
+    const amigo = input.value.trim().toUpperCase();   // Esta función toma el valor del input, lo convierte a mayúsculas y lo agrega a la lista
+   
+    if (flagNuevoSorteo) {
+        resetearJuego(); // Reiniciar el juego si se ha realizado un nuevo sorteo       
+        flagNuevoSorteo = false; // Cambiar la bandera para indicar que se ha realizado un sorteo}
+    } 
+    
+    if (validarNombre(amigo)) { // asegura de que el nombre solo contenga letras y no esté vacío
         poblarListadoAmigos(amigo); // Llamar a la función para poblar la lista de amigos|          
     } else {
         alert('Por favor, ingresa un nombre válido.');
     }   
     input.value = ''; 
-    obtenerListadoAmigos(); // Actualizar la lista de amigos
+    actualizarListadoAmigos(); // Actualizar la lista de amigos en el DOM
 };
 
+// Función para validar el nombre del amigo
+// Esta función utiliza una expresión regular para asegurarse de que el nombre solo contenga letras
 function validarNombre(texto) {
-  const regex = /^[a-zA-Z]+$/;
+  const regex = /^[A-ZÁÉÍÓÚ]+$/;
   return regex.test(texto);
 }
 
-function poblarListadoAmigos(amigo) {   
-   
+// Función para poblar la lista de amigos
+// Esta función verifica si el amigo ya está en la lista antes de agregarlo
+function poblarListadoAmigos(amigo) {      
         if (amigosListados.includes(amigo)) {
             alert('Este amigo ya está en la lista.');
             return;
@@ -67,12 +68,33 @@ function poblarListadoAmigos(amigo) {
         amigosListados.push(amigo); // Agregar el amigo a la lista
 }
 
-// Función para mostrar el resultado del sorteo
-// Esta función selecciona un amigo al azar de la lista y muestra el resultado en la interfaz
+// Función para acutalizar DOM de lista de amigos
+// Esta función recorre los elementos del array y los almacena en un lista HTML
+function actualizarListadoAmigos() {
+    lista.textContent = ''; // Limpiar la lista antes de poblarla 
+    amigosListados.sort(); // Ordenar la lista de amigos alfabéticamente  
+        for (let i = 0; i < amigosListados.length; i++) {
+        const itemLista = document.createElement('li');
+        itemLista.textContent = amigosListados[i];
+        lista.appendChild(itemLista); 
+    }     
+          
+}
 
+// Función para reiniciar el juego
+// Esta función restablece el título, limpia los resultados previos y vacía la lista
+function resetearJuego() {
+    titular.textContent = 'Digite el nombre de sus amigos!';
+    resultados.textContent = ''; // Limpiar resultados previos
+    lista.textContent = ''; // Limpiar la lista de amigos
+    divProgreso.style.visibility = "hidden"; // Ocultar la barra de progreso
+    while (amigosListados.length > 0) {// Limpiar la lista de amigos
+        amigosListados.pop();
+    }   
+}
 
 // Función para animar la barra de progreso
-// Esta función utiliza setInterval para incrementar el ancho de la barra de progreso hasta un valor final
+// Esta función toma el ancho de la barra de progreso y anima su crecimiento hasta ese ancho
 function animateProgressBar(width) {
     divProgreso.style.visibility = "visible"; // Hacer visible la barra de progreso
     const progressBar = document.getElementById("animaSorteo");
@@ -95,15 +117,3 @@ function animateProgressBar(width) {
         }
     }, stepTime);
 }
-
-// Función para reiniciar el juego
-// Esta función restablece el título, limpia los resultados previos y vacía la lista
-function resetearJuego() {
-    titular.textContent = 'Digite el nombre de sus amigos!';
-    resultados.textContent = ''; // Limpiar resultados previos
-    while (amigosListados.length > 0) {// Limpiar la lista de amigos
-        amigosListados.pop();
-    }
-    lista.textContent = ''; // Limpiar la lista de amigos
-}
-
